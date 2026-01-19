1fa2adfcf8fa0025ad39f1e859f9f49ba32ca16c
Chenxiang Zhang <chenxiang.zhang@uni.lu>
2026-01-19 12:29:47 +0100

    fix interpolation, test webloader

--- imagenet1k/data.py
+++ imagenet1k/data.py
@@ -7,6 +7,7 @@ import os
 import torch.distributed as dist
 import torchvision.transforms as T
 import webdataset as wds
+from PIL import Image
 from timm.data.auto_augment import rand_augment_transform
 from torch.utils.data import DataLoader
 
@@ -21,6 +22,7 @@ def get_dataloaders(
     distributed=True,
 ):
     world_size = dist.get_world_size() if dist.is_initialized() else 1
+    steps_per_epoch = IMAGENET_TRAIN_SAMPLES // (batch_size_per_gpu * world_size)
 
     tr_transform = T.Compose(
         [
@@ -28,7 +30,10 @@ def get_dataloaders(
             T.RandomHorizontalFlip(),
             rand_augment_transform(
                 config_str="rand-m10-n2",
-                hparams={"translate_const": 100, "img_mean": (128, 128, 128)},
+                hparams={
+                    "img_mean": (128, 128, 128),
+                    "interpolation": Image.BILINEAR,
+                },
             ),
             T.ToTensor(),
             T.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
@@ -45,12 +50,26 @@ def get_dataloaders(
     tr_dataset = (
         wds.WebDataset(
             os.path.join(dir_data, "imagenet1k-train-{0000..1023}.tar"),
+            resampled=True,
             shardshuffle=True,
             nodesplitter=wds.split_by_node,
         )
         .shuffle(250_000)
         .decode("pil")
         .map(lambda x: (tr_transform(x["jpg"]), int(x["cls"])))
+    )
+    tr_loader = (
+        wds.WebLoader(  # wraps DataLoader
+            tr_dataset,
+            batch_size=None,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            persistent_workers=True,
+        )
+        .unbatched()  # unbatch-shuffle-rebatch for cross-worker mixing
+        .shuffle(5000)
+        .batched(batch_size_per_gpu)
+        .with_epoch(steps_per_epoch)
     )
     vl_dataset = (
         wds.WebDataset(
@@ -61,14 +80,6 @@ def get_dataloaders(
         .decode("pil")
         .map(lambda x: (vl_transform(x["jpg"]), int(x["cls"])))
     )
-    tr_loader = DataLoader(
-        tr_dataset,
-        batch_size=batch_size_per_gpu,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
-        persistent_workers=True,
-        drop_last=True,
-    )
     vl_loader = DataLoader(
         vl_dataset,
         batch_size=batch_size_per_gpu,
@@ -77,5 +88,4 @@ def get_dataloaders(
         persistent_workers=True,
     )
 
-    steps_per_epoch = IMAGENET_TRAIN_SAMPLES // (batch_size_per_gpu * world_size)
     return tr_loader, vl_loader, steps_per_epoch
